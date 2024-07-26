6955407cec70589cac0f983ead58c6ce0c04c6b8
Holodun406 <81082714+Holodun406@users.noreply.github.com>
2024-07-26 10:08:13 +0300

    Update contract-07f5b7650b.sol

--- contract-07f5b7650b.sol
+++ contract-07f5b7650b.sol
@@ -2,9 +2,11 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract MirroxToken is ERC20, ERC20Permit {
+    constructor() ERC20("MirroxToken", "MRX") ERC20Permit("MirroxToken") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
 }
